913cfa303d3393f1238e1d32e95f7ccf12243e0e
SW-GAGAN <srgaganreddy0@gmail.com>
2021-11-10 19:30:46 +0530

    Revert "Added: component creation of forget-password"
    This reverts commit 23ed63e5eb1b40fe1d3672a88346dfc0613bf523.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ForgetPasswordComponent } from './Component/User/authentication/forget-password/forget-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ForgetPasswordComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
